1de51fbf438b14e11e4e9467279a8f34fd03b174
ADITYA CHOUDHARY <adityachoudhary3003@gmail.com>
2019-01-16 18:32:04 +0530

    Added .lecture1

---
+++ lecture1/src/Drill.java
@@ -0,0 +1,5 @@
+DRILL  
+Procedural Approach for Vacation
+Find a bag to put carry required things on trip
+collect all the required things and put them in the bag 
+put the mostly required things in front pocket
