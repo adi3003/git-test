914178d0ba8e0c0c24e107faabd0df8af1d33f23
ADITYA CHOUDHARY <adityachoudhary3003@gmail.com>
2019-01-16 19:17:05 +0530

    Added .lecture2

---
+++ lecture2/p1/src/DisplayHello.java
@@ -0,0 +1,17 @@
+class DisplayHello
+
+{
+
+	public static void main (String[]args)
+
+	{ 	//To print hello
+
+		
+
+		System.out.print("Hello ");
+
+		System.out.println("Hello World!");
+
+	}
+
+}
