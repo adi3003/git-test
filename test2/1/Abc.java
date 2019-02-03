466e2a2985a1be8a99e563bb9890dd4fd2dec91d
adi3003 <46483553+adi3003@users.noreply.github.com>
2019-02-03 22:47:00 +0530

    Add files via upload

---
+++ test2/1/Abc.java
@@ -0,0 +1,21 @@
+public class Abc{
+	protected int x;
+	Abc()
+	{
+		x=2;
+
+	}
+}
+public class abcex extend abc{
+	void display(){
+		System.out.println(x;)
+	}
+}
+class abctest {
+	public static void main(String[]args ){
+		abcex = new abcex();
+		a.display();
+		System.out.println(a.xs)
+	}
+
+}
